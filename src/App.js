02b3113be1a40dee218030978b774fa0a198c9a7
MrGrigoryAlexandrovich <ahmedcvrle@gmail.com>
2021-06-20 12:06:38 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './css/App.css';
 import Navigation from './components/Navigation'
+import StartPage from './components/StartPage'
+import CategoryPage from './components/CategoryPage'
+import ProductPage from './components/ProductPage'
+import CheckoutPage from './components/CheckoutPage'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navigation/>
+                <Switch>
+                    <Route path="/" exact>
+                        <StartPage/>
+                    </Route>
+                    <Route path="/categorypage">
+                        <CategoryPage/>
+                    </Route>
+                    <Route path="/productpage">
+                        <ProductPage/>
+                    </Route>
+                    <Route path="/checkoutpage">
+                        <CheckoutPage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
