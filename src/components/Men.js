429bf14a7aa13dfa1844cd4c4c1c54bbba13eb2b
MrGrigoryAlexandrovich <ahmedcvrle@gmail.com>
2021-06-23 08:25:33 +0200

    Men page added

--- src/components/Men.js
+++ src/components/Men.js
@@ -1,9 +1,55 @@
 import React from 'react'
-
+import '../css/Men.css'
 export default function Men() {
     return (
-        <div>
-                <h1>Men</h1>
+        <div className="Men">
+              <span id="title">Men</span>
+              <img alt='' className="men1"/>
+              <img alt='' className="men2"/>
+              <img alt='' className="men3"/>
+              <img alt='' className="men4"/>
+              <img alt='' className="men1"/>
+              <img alt='' className="men2"/>
+              <img alt='' className="men3"/>
+              <img alt='' className="men4"/>
+              <img alt='' className="men1"/>             
+              <img alt='' className="men2"/>            
+              <img alt='' className="men3"/>             
+              <img alt='' className="men4"/>            
+              <img alt='' className="men1"/>
+              <img alt='' className="men2"/>
+              <img alt='' className="men3"/>
+              <img alt='' className="men4"/>
+               <p className="TextStyle" id="textposition1">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition2">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition3">RECYCLED POLYESTER PERFORMANCE PARKA</p> 
+               <p className="TextStyle" id="textposition4">RECYCLED POLYESTER PERFORMANCE PARKA</p> 
+               <p className="TextStyle" id="textposition5">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition6">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition7">RECYCLED POLYESTER PERFORMANCE PARKA</p> 
+               <p className="TextStyle" id="textposition8">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition9">RECYCLED POLYESTER PERFORMANCE PARKA</p> 
+               <p className="TextStyle" id="textposition10">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition11">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition12">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition13">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="TextStyle" id="textposition15">RECYCLED POLYESTER PERFORMANCE PARKA</p>
+               <p className="PriceStyle" id="priceposition1">€135</p>
+               <p className="PriceStyle" id="priceposition2">€135</p>
+               <p className="PriceStyle" id="priceposition3">€135</p>
+               <p className="PriceStyle2" id="priceposition4">€99</p>
+               <p className="PriceStyle" id="priceposition44">€135</p>
+               <p className="PriceStyle2" id="priceposition5" >€99</p>  
+               <p className="PriceStyle" id="priceposition55" >€135</p>
+               <p className="PriceStyle" id="priceposition6">€135</p>
+               <p className="PriceStyle" id="priceposition7">€135</p>
+               <p className="PriceStyle" id="priceposition8">€135</p>
+               <p className="PriceStyle" id="priceposition9">€135</p>
+               <p className="PriceStyle" id="priceposition10">€135</p>
+               <p className="PriceStyle" id="priceposition11">€135</p>
+               <p className="PriceStyle" id="priceposition12">€135</p>
+               <p className="PriceStyle" id="priceposition13">€135</p>
+               <p className="PriceStyle" id="priceposition15">€135</p>
         </div>
     )
 }
